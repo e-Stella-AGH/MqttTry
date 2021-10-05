caf7f5a74c046a5dc1a2193cb86ca7df3c57e710
Dawid Dębowski <dawid199960@gmail.com>
2021-10-05 22:14:37 +0200

    code editor

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
-import { Other } from './Other';
 import { Realtime } from "ably/browser/static/ably-commonjs.js"
+import { CodeEditor } from 'e-stella-code-editor'
 
-const ably = new Realtime({ key: 'vVogsQ.TIFtUw:VOMpWmEg9kv7tR7K' })
+const App = () => {
 
-function App() {
+  const id = Math.floor(Math.random()*100)
 
-  const channel = ably.channels.get('newChannel')
+  const ably = new Realtime({ key: process.env.REACT_APP_ABLY_KEY })
+  const channel = ably.channels.get('code')
+
+  const sub = (func) => channel.subscribe(func)
+  const pub = (data) => channel.publish('codeChanged', data, (err) => err ? console.log(err) : console.log('ok'))
 
   return (
-    <Other channel={channel} />      
-  );
+    <div style={{overflowX: 'hidden', overflowY: 'hidden'}}>
+      <CodeEditor
+        fetchTasks={
+          () => fetch("https://recruitment-service-estella.herokuapp.com/api/tasks?process=16").then(response => response.json())
+        }
+        codeCheckerBaseLink="https://e-stella-code-executor.herokuapp.com"
+        sharingCodeFunctions={{ sub, pub, id }}
+      />
+    </div>
+  )
 }
 
 export default App;
